0421240d7ba152c43e4548e171be70b2c76ae3f1
denise.worthen <Denise.Worthen@noaa.gov>
2023-10-06 15:14:42 +0000

    working version
    * comparison to 1/4 deg rect created by NCL
    2:55: RMS SSH                              3.7417E-08            NORMALIZED  5.6243E-08
    4:64: RMS SST                              6.9100E-07            NORMALIZED  4.6913E-08
    6:73: RMS SSS                              1.5883E-06            NORMALIZED  4.6559E-08
    8:82: RMS speed                            9.7695E-09            NORMALIZED  5.6892E-08
    10:91: RMS ePBL                             2.0080E-06            NORMALIZED  5.7603E-08
    12:100: RMS MLD_003                          4.4865E-06            NORMALIZED  8.5575E-08
    14:109: RMS MLD_0125                         1.0973E-05            NORMALIZED  1.0762E-07
    16:118: RMS SSU                              9.5967E-09            NORMALIZED  7.9765E-08
    18:127: RMS SSV                              6.2452E-09            NORMALIZED  7.0967E-08
    20:136: RMS latent                           5.4319E-06            NORMALIZED  5.6156E-08
    22:145: RMS sensible                         1.2109E-06            NORMALIZED  7.6210E-08
    24:154: RMS SW                               1.1993E-05            NORMALIZED  7.4920E-08
    26:163: RMS LW                               2.5634E-06            NORMALIZED  5.1779E-08
    28:172: RMS evap                             2.2044E-12            NORMALIZED  5.7339E-08
    30:181: RMS lprec                            4.2114E-12            NORMALIZED  1.6372E-07
    32:190: RMS fprec                            6.4795E-13            NORMALIZED  3.1062E-07
    34:199: RMS LwLatSens                        8.3298E-06            NORMALIZED  5.2588E-08
    36:208: RMS Heat_PmE                         5.6015E-07            NORMALIZED  8.6874E-08
    38:217: RMS taux                             8.0290E-09            NORMALIZED  9.8891E-08
    40:226: RMS tauy                             5.4570E-09            NORMALIZED  8.5749E-08
    42:235: RMS temp                             4.9692E-07            NORMALIZED  5.4994E-08
    44:244: RMS so                               1.6489E-06            NORMALIZED  4.7015E-08
    46:253: RMS uo                               5.5913E-09            NORMALIZED  9.9163E-08
    48:262: RMS vo                               3.6671E-09            NORMALIZED  8.6458E-08

---
+++ ocnpost.F90
@@ -0,0 +1,376 @@
+program ocnpost
+
+  use utils_mod
+  use ocnvars
+  use netcdf
+
+  implicit none
+
+  character(len=240) :: filesrc, filedst, wgtsfile, fout
+  !character(len=120) :: wgtsdir = '/scratch1/NCEPDEV/climate/Denise.Worthen/grids-test-202210/'
+  character(len=120) :: wgtsdir = '/scratch1/NCEPDEV/climate/climpara/S2S/FIX/fix_UFSp6/fix_reg2grb2/'
+  ! source grid, tripole 1/4 deg, 40 vertical levels
+  integer, parameter :: nxt = 1440, nyt = 1080, nlevs = 40
+
+  ! destination grids
+  integer, parameter :: ndest = 3
+  integer, parameter, dimension(ndest) :: nxrs = (/1440, 720, 360/)
+  integer, parameter, dimension(ndest) :: nyrs = (/ 721, 361, 181/)
+  character(len=4), dimension(ndest)   :: dstgrds = (/'0p25', '0p5 ', '1p0 '/)
+
+  ! packed arrays on source grid
+  real, allocatable, dimension(:,:)   :: bilin2d, consd2d
+  real, allocatable, dimension(:,:,:) :: bilin3d
+  ! variable types
+  type(vardefs), allocatable :: b2d(:)
+  type(vardefs), allocatable :: c2d(:)
+  type(vardefs), allocatable :: b3d(:)
+
+  ! source grid fields
+  real, dimension(nxt,nyt,nlevs) :: tmp3d
+  real, dimension(nxt*nyt,nlevs) :: mask3d
+  real, dimension(nxt*nyt)       :: cosrot, sinrot
+  real, dimension(nlevs)         :: z_l
+  real, dimension(0:nlevs)       :: z_i
+
+  ! destination grid fields, vectors
+  real, allocatable, dimension(:,:)  :: dstlon, dstlat
+  real, allocatable, dimension(:,:)  :: rgmask3d
+  ! output fields (x,y,z)
+  real, allocatable, dimension(:)     :: out1d
+  real, allocatable, dimension(:,:)   :: out2d
+  real, allocatable, dimension(:,:,:) :: out3d
+  ! packed remapped fields
+  real, allocatable, dimension(:,:)   :: rgb2d, rgc2d
+  real, allocatable, dimension(:,:,:) :: rgb3d
+
+  real(kind=8) :: timestamp
+  character(len= 40) :: timeunit, timecal
+  character(len= 20) :: vname, vunit
+  character(len=120) :: vlong
+  character(len=4)   :: dstgrid
+
+  integer :: nd, nxr, nyr
+  integer :: i,j,k,n,nn,nvalid
+  integer :: rc,ncid,varid,dimid
+  integer :: nbilin2d, nbilin3d, nconsd2d
+  real    :: vfill
+  integer :: idimid, jdimid, kdimid, edimid, timid
+
+  logical :: debug = .true.
+
+  ! initialize the variable type
+  call ovars_typedefine
+
+  ! --------------------------------------------------------
+  ! read the source file and obtain the units and long name,
+  ! rotation angles, vertical grid and time axis
+  ! --------------------------------------------------------
+
+  filesrc = '/scratch1/NCEPDEV/climate/Jiande.Wang/For-others/For-Eric/ocn2013100106.01.2013100100.nc'
+  !filesrc='ocn_2013_04_01_03.nc'
+
+  nvalid = 0
+  rc = nf90_open(trim(filesrc), nf90_nowrite, ncid)
+  do i = 1,maxvars
+     if (len_trim(ovars(i)%input_var_name) > 0 ) then
+        rc = nf90_inq_varid(ncid, trim(ovars(i)%input_var_name), varid)
+        rc = nf90_get_att(ncid, varid, 'long_name', ovars(i)%long_name)
+        rc = nf90_get_att(ncid, varid, 'units', ovars(i)%units)
+        rc = nf90_get_att(ncid, varid, '_FillValue', ovars(i)%var_fillvalue)
+        nvalid = nvalid+1
+        if (trim(ovars(i)%input_var_name) == 'temp')vfill = ovars(i)%var_fillvalue
+     end if
+  end do
+
+  ! timestamp
+  rc = nf90_inq_varid(ncid, 'time', varid)
+  rc = nf90_get_var(ncid, varid, timestamp)
+  rc = nf90_get_att(ncid, varid, 'units', timeunit)
+  rc = nf90_get_att(ncid, varid, 'calendar', timecal)
+  ! cell centers
+  rc = nf90_inq_varid(ncid, 'z_l', varid)
+  rc = nf90_get_var(ncid, varid, z_l)
+  ! cell edges
+  rc = nf90_inq_varid(ncid, 'z_i', varid)
+  rc = nf90_get_var(ncid, varid, z_i)
+  rc = nf90_close(ncid)
+  ! rotation angles
+  call getfield(trim(filesrc), 'cos_rot', dims=(/nxt,nyt/), field=cosrot)
+  call getfield(trim(filesrc), 'sin_rot', dims=(/nxt,nyt/), field=sinrot)
+  !print *,z_l
+  !print *,z_i
+  !print *,timestamp
+
+  ! --------------------------------------------------------
+  ! mask3d contain 1's on land and 0's at valid points.
+  ! when remapped, any mask value > 0 identifies land values that
+  ! have crept into the field. remapped model fields are then
+  ! masked with this interpolation mask
+  ! --------------------------------------------------------
+
+  rc = nf90_open(trim(filesrc), nf90_nowrite, ncid)
+  ! 3D temp to use as mask
+  rc = nf90_inq_varid(ncid, 'temp', varid)
+  rc = nf90_get_var(ncid, varid, tmp3d)
+  rc = nf90_close(ncid)
+
+  mask3d = reshape(tmp3d, (/nxt*nyt,nlevs/))
+  ! set mask to 0 on ocean, 1 on land on source grid
+  where(mask3d .eq. vfill)mask3d = 1.0
+  where(mask3d .ne.   1.0)mask3d = 0.0
+  if (debug) print '(a,2g14.7)',' mask min/max on source grid ',minval(mask3d),maxval(mask3d)
+  if (debug) call dumpnc('mask3d.nc', 'mask3d', dims=(/nxt,nyt,nlevs/), field=mask3d)
+
+  ! --------------------------------------------------------
+  ! count numbers of fields to remapped for each
+  ! mapping type; these can be remapped as packed arrays
+  ! --------------------------------------------------------
+
+  nbilin2d = 0; nbilin3d = 0; nconsd2d = 0
+  do n = 1,nvalid
+     if (trim(ovars(n)%var_remapmethod)  == 'bilinear') then
+        if (ovars(n)%var_dimen == 2) nbilin2d = nbilin2d + 1
+        if (ovars(n)%var_dimen == 3) nbilin3d = nbilin3d + 1
+     end if
+     if (trim(ovars(n)%var_remapmethod)  == 'conserve')nconsd2d = nconsd2d + 1  !no 3d variables w/ conservative mapping
+  end do
+  if (debug) print '(3(a,i4))','bilin 2d ',nbilin2d,' bilin 3d ',nbilin3d,' conserv 2d ',nconsd2d
+
+  ! initialization required when compile in sinit_arrays=nan
+  allocate(bilin2d(nxt*nyt,nbilin2d)); bilin2d = 0.0
+  allocate(consd2d(nxt*nyt,nconsd2d)); consd2d = 0.0
+  allocate(bilin3d(nxt*nyt,nlevs,nbilin3d)); bilin3d = 0.0
+
+  allocate(b2d(1:nbilin2d))
+  allocate(c2d(1:nconsd2d))
+  allocate(b3d(1:nbilin3d))
+
+  ! --------------------------------------------------------
+  ! create types for each packed array
+  ! --------------------------------------------------------
+
+  i = 0; j = 0; k = 0
+  do n = 1,nvalid
+     if (trim(ovars(n)%var_remapmethod)  == 'bilinear') then
+        if (ovars(n)%var_dimen == 2) then
+           i = i+1; b2d(i) = ovars(n)
+        end if
+        if (ovars(n)%var_dimen == 3) then
+           j = j+1; b3d(j) = ovars(n)
+        end if
+     end if
+     if (trim(ovars(n)%var_remapmethod)  == 'conserve') then
+        k = k+1; c2d(k) = ovars(n)
+     end if
+  end do
+
+  if (debug) then
+     print *,'2D fields mapped bilin'
+     do n = 1,nbilin2d
+        print '(i6,4(a,a))',n,'  ',trim(b2d(n)%input_var_name),'  ',trim(b2d(n)%var_grid), &
+             '  ',trim(b2d(n)%var_pair),'  ', trim(b2d(n)%var_pair_grid)
+     end do
+     print *,'3D fields mapped bilin'
+     do n = 1,nbilin3d
+        print '(i6,4(a,a))',n,'  ',trim(b3d(n)%input_var_name),'  ',trim(b3d(n)%var_grid), &
+             '  ',trim(b3d(n)%var_pair),'  ', trim(b3d(n)%var_pair_grid)
+     end do
+     print *,'2D fields mapped conserv'
+     do n = 1,nconsd2d
+        print '(i6,4(a,a))',n,'  ',trim(c2d(n)%input_var_name),'  ',trim(c2d(n)%var_grid), &
+             '  ',trim(c2d(n)%var_pair),'  ', trim(c2d(n)%var_pair_grid)
+     end do
+  end if
+
+  ! --------------------------------------------------------
+  ! create packed arrays for mapping
+  ! --------------------------------------------------------
+
+  ! 2D bilin
+  call packarrays(trim(filesrc), trim(wgtsdir), cosrot, sinrot, b2d, dims=(/nxt,nyt/), nflds=nbilin2d, fields=bilin2d)
+  if (debug) call dumpnc('bilin2d.nc', 'bilin2d', dims=(/nxt,nyt/), nflds=nbilin2d, field=bilin2d)
+
+  ! 2D conserv
+  call packarrays(trim(filesrc), trim(wgtsdir), cosrot, sinrot, c2d, dims=(/nxt,nyt/), nflds=nconsd2d, fields=consd2d)
+  if (debug) call dumpnc('consd2d.nc', 'consd2d', dims=(/nxt,nyt/), nflds=nconsd2d, field=consd2d)
+
+  ! 3D bilin
+  call packarrays(trim(filesrc), trim(wgtsdir), cosrot, sinrot, b3d, dims=(/nxt,nyt,nlevs/), nflds=nbilin3d, fields=bilin3d)
+  if (debug) call dumpnc('bilin3d.nc', 'bilin3d', dims=(/nxt,nyt,nlevs/), nk=nlevs, nflds=nbilin3d, field=bilin3d)
+
+  ! --------------------------------------------------------
+  ! remap packed arrays to each destination grid
+  ! --------------------------------------------------------
+
+  nd = 1
+  !do nd = 1,ndest
+     dstgrid =  trim(dstgrds(nd))
+     nxr = nxrs(nd); nyr = nyrs(nd)
+
+     allocate(rgb2d(nxr*nyr,nbilin2d)); rgb2d = 0.0
+     allocate(rgc2d(nxr*nyr,nconsd2d)); rgc2d = 0.0
+     allocate(rgb3d(nxr*nyr,nlevs,nbilin3d)); rgb3d = 0.0
+     allocate(dstlon(nxr,nyr)); dstlon = 0.0
+     allocate(dstlat(nxr,nyr)); dstlat = 0.0
+     allocate(out1d(nxr*nyr)); out1d = 0.0
+     allocate(rgmask3d(nxr*nyr,nlevs)); rgmask3d = 0.0
+
+     ! lat,lon of destination grid can be obtained from xc_b,yc_b in wgtsfile
+     wgtsfile = trim(wgtsdir)//'tripole.mx025.Ct.to.rect.'//trim(dstgrid)//'.bilinear.nc'
+     rc = nf90_open(trim(wgtsfile), nf90_nowrite, ncid)
+     rc = nf90_inq_varid(ncid, 'xc_b', varid)
+     print *,trim(nf90_strerror(rc))
+     rc = nf90_get_var(ncid,    varid, out1d)
+     dstlon = reshape(out1d,(/nxr,nyr/))
+     rc = nf90_inq_varid(ncid, 'yc_b', varid)
+     print *,trim(nf90_strerror(rc))
+     rc = nf90_get_var(ncid,    varid, out1d)
+     dstlat = reshape(out1d,(/nxr,nyr/))
+     rc = nf90_close(ncid)
+
+     wgtsfile = trim(wgtsdir)//'tripole.mx025.Ct.to.rect.'//trim(dstgrid)//'.bilinear.nc'
+     if (debug) print '(a)','remapping 2D fields bilinear with '//trim(wgtsfile)
+     call remap(trim(wgtsfile), dim2=nbilin2d, src_field=bilin2d, dst_field=rgb2d)
+     if (debug) call dumpnc('rgbilin2d.'//dstgrid//'.nc', 'rgbilin2d', dims=(/nxr,nyr/), nflds=nbilin2d, field=rgb2d)
+
+     wgtsfile = trim(wgtsdir)//'tripole.mx025.Ct.to.rect.'//trim(dstgrid)//'.conserve.nc'
+     if (debug) print '(a)','remapping 2D fields conserv with '//trim(wgtsfile)
+     call remap(trim(wgtsfile), dim2=nconsd2d, src_field=consd2d, dst_field=rgc2d)
+     if (debug) call dumpnc('rgconsd2d.'//dstgrid//'.nc', 'rgconsd2d', dims=(/nxr,nyr/), nflds=nconsd2d, field=rgc2d)
+
+     wgtsfile = trim(wgtsdir)//'tripole.mx025.Ct.to.rect.'//trim(dstgrid)//'.bilinear.nc'
+     if (debug) print '(a)','remapping 3D fields bilinear with '//trim(wgtsfile)
+     call remap(trim(wgtsfile), nk=nlevs, nflds=nbilin3d, src_field=bilin3d, dst_field=rgb3d)
+     if (debug) call dumpnc('rgbilin3d.'//dstgrid//'.nc', 'rgbilin3d', dims=(/nxr,nyr,nlevs/), nk=nlevs, nflds=nbilin3d, field=rgb3d)
+
+     ! --------------------------------------------------------
+     ! remap the source grid 3D mask to obtain the interpolation mask.
+     ! --------------------------------------------------------
+
+     wgtsfile = trim(wgtsdir)//'tripole.mx025.Ct.to.rect.'//trim(dstgrid)//'.bilinear.nc'
+     call remap(trim(wgtsfile), dim2=nlevs, src_field=mask3d, dst_field=rgmask3d)
+     ! set interpolation mask missing on land, 1.0 on ocean on destination grids
+     where(rgmask3d > 0.0)rgmask3d = vfill
+     where(rgmask3d /= vfill)rgmask3d = 1.0
+     if (debug) print '(a,2g14.7)',' mask min/max on destination grid ',minval(rgmask3d),maxval(rgmask3d)
+     if (debug) call dumpnc('rgmask3d.nc', 'rgmask3d', dims=(/nxr,nyr,nlevs/), field=rgmask3d)
+
+     ! --------------------------------------------------------
+     ! mask the mapped fields
+     ! --------------------------------------------------------
+
+     do n = 1,nbilin2d
+        where(rgmask3d(:,1) .eq. vfill)rgb2d(:,n) = vfill
+     end do
+     do n = 1,nconsd2d
+        where(rgmask3d(:,1) .eq. vfill)rgc2d(:,n) = vfill
+     end do
+     do n = 1,nbilin3d
+        where(rgmask3d(:,:) .eq. vfill)rgb3d(:,:,n) = vfill
+     end do
+
+     ! --------------------------------------------------------
+     ! write the mapped fields
+     ! --------------------------------------------------------
+
+     allocate(out2d(nxr,nyr)); out2d = 0.0
+     allocate(out3d(nxr,nyr,nlevs)); out3d = 0.0
+
+     fout = 'test.'//dstgrid//'.nc'
+
+     rc = nf90_create(trim(fout), nf90_clobber, ncid)
+     rc = nf90_def_dim(ncid, 'longitude', nxr, idimid)
+     rc = nf90_def_dim(ncid, 'latitude',  nyr, jdimid)
+     rc = nf90_def_dim(ncid,  'z_l',    nlevs, kdimid)
+     rc = nf90_def_dim(ncid,  'z_i',  nlevs+1, edimid)
+     rc = nf90_def_dim(ncid, 'time', nf90_unlimited, timid)
+
+     ! define the time variable
+     rc = nf90_def_var(ncid, 'time', nf90_double, (/timid/), varid)
+     rc = nf90_put_att(ncid, varid, 'units', trim(timeunit))
+     rc= nf90_put_att(ncid, varid, 'calendar', trim(timecal))
+     ! spatial grid
+     rc = nf90_def_var(ncid, 'longitude', nf90_float,  (/idimid/), varid)
+     rc = nf90_put_att(ncid, varid, 'units', 'degrees_east')
+     rc = nf90_def_var(ncid, 'latitude', nf90_float,  (/jdimid/), varid)
+     rc = nf90_put_att(ncid, varid, 'units', 'degrees_north')
+     ! vertical grid
+     rc = nf90_def_var(ncid, 'z_l', nf90_float,  (/kdimid/), varid)
+     rc = nf90_put_att(ncid, varid, 'units', 'm')
+     rc = nf90_put_att(ncid, varid, 'positive', 'down')
+     rc = nf90_def_var(ncid, 'z_i', nf90_float,  (/edimid/), varid)
+     rc = nf90_put_att(ncid, varid, 'units', 'm')
+     rc = nf90_put_att(ncid, varid, 'positive', 'down')
+
+     do n = 1,nbilin2d
+        vname = trim(b2d(n)%output_var_name)
+        vunit = trim(b2d(n)%units)
+        vlong = trim(b2d(n)%long_name)
+        vfill = b2d(n)%var_fillvalue
+        rc = nf90_def_var(ncid, vname, nf90_float, (/idimid,jdimid,timid/), varid)
+        rc = nf90_put_att(ncid, varid,     'units',  vunit)
+        rc = nf90_put_att(ncid, varid, 'long_name',  vlong)
+        rc = nf90_put_att(ncid, varid, '_FillValue', vfill)
+     enddo
+     do n = 1,nconsd2d
+        vname = trim(c2d(n)%output_var_name)
+        vunit = trim(c2d(n)%units)
+        vlong = trim(c2d(n)%long_name)
+        vfill = c2d(n)%var_fillvalue
+        rc = nf90_def_var(ncid, vname, nf90_float, (/idimid,jdimid,timid/), varid)
+        rc = nf90_put_att(ncid, varid,     'units',  vunit)
+        rc = nf90_put_att(ncid, varid, 'long_name',  vlong)
+        rc = nf90_put_att(ncid, varid, '_FillValue', vfill)
+     enddo
+     do n = 1,nbilin3d
+        vname = trim(b3d(n)%output_var_name)
+        vunit = trim(b3d(n)%units)
+        vlong = trim(b3d(n)%long_name)
+        vfill = b3d(n)%var_fillvalue
+        rc = nf90_def_var(ncid, vname, nf90_float, (/idimid,jdimid,kdimid,timid/), varid)
+        rc = nf90_put_att(ncid, varid,     'units',  vunit)
+        rc = nf90_put_att(ncid, varid, 'long_name',  vlong)
+        rc = nf90_put_att(ncid, varid, '_FillValue', vfill)
+     enddo
+     rc = nf90_enddef(ncid)
+
+     ! dimensions
+     rc = nf90_inq_varid(ncid, 'z_l', varid)
+     rc = nf90_put_var(ncid,   varid,   z_l)
+     rc = nf90_inq_varid(ncid, 'z_i', varid)
+     rc = nf90_put_var(ncid,   varid,   z_i)
+     rc = nf90_inq_varid(ncid, 'longitude', varid)
+     rc = nf90_put_var(ncid,   varid, dstlon(:,1))
+     rc = nf90_inq_varid(ncid,  'latitude', varid)
+     rc = nf90_put_var(ncid,   varid, dstlat(1,:))
+     ! time
+     rc = nf90_inq_varid(ncid, 'time', varid)
+     rc = nf90_put_var(ncid, varid, timestamp)
+
+     do n = 1,nbilin2d
+        out2d(:,:) = reshape(rgb2d(:,n), (/nxr,nyr/))
+        vname = trim(b2d(n)%output_var_name)
+        rc = nf90_inq_varid(ncid, vname, varid)
+        rc = nf90_put_var(ncid,   varid, out2d)
+     end do
+     do n = 1,nconsd2d
+        out2d(:,:) = reshape(rgc2d(:,n), (/nxr,nyr/))
+        vname = trim(c2d(n)%output_var_name)
+        rc = nf90_inq_varid(ncid, vname, varid)
+        rc = nf90_put_var(ncid,   varid, out2d)
+     end do
+     do n = 1,nbilin3d
+        out3d(:,:,:) = reshape(rgb3d(:,:,n), (/nxr,nyr,nlevs/))
+        vname = trim(b3d(n)%output_var_name)
+        rc = nf90_inq_varid(ncid, vname, varid)
+        rc = nf90_put_var(ncid,   varid, out3d)
+     end do
+     rc = nf90_close(ncid)
+
+     deallocate(rgb2d, rgc2d, rgb3d)
+     deallocate(out1d, dstlon, dstlat, rgmask3d)
+  !end do !nd
+  print *,'all done!'
+
+end program ocnpost
---
+++ ocnvars.F90
@@ -0,0 +1,106 @@
+module ocnvars
+
+  implicit none
+
+  integer, parameter :: maxvars = 40          !< The maximum number of variables written to a file
+
+  type :: vardefs
+     character(len= 10)   :: input_var_name    !< A variable's  input variable name
+     character(len= 10)   :: output_var_name   !< A variable's output variable name
+     character(len=120)   :: long_name         !< A variable's long name
+     character(len= 20)   :: units             !< A variable's unit
+     character(len= 10)   :: var_remapmethod   !< A variable's mapping method
+     integer              :: var_dimen         !< A variable's dimensionality
+     character(len=  2)   :: var_grid          !< A variable's input grid location; all output locations are on cell centers
+     character(len= 10)   :: var_pair          !< A variable's pair
+     character(len=  2)   :: var_pair_grid     !< A pair variable grid
+     real                 :: var_fillvalue     !< A variable's fillvalue
+  end type vardefs
+
+  type(vardefs), public :: ovars(maxvars)   !< Attribute definitions for the variables
+
+contains
+
+  subroutine ovars_typedefine
+
+    ! local variables
+    integer :: ii = 0
+
+    !set defaults
+    ovars(:)%input_var_name=''
+    ovars(:)%var_grid  = 'Ct'
+    ovars(:)%var_remapmethod  = 'bilinear'
+    ovars(:)%var_dimen = 2
+    ovars(:)%var_pair = ''
+    ovars(:)%var_pair_grid = ''
+    ovars(:)%long_name = ''          ! obtained from input file
+    ovars(:)%units = ''              ! obtained from input file
+    ovars(:)%var_fillvalue = -1.0    ! obtained from input file
+
+    ! 2D states with native grid location on cell centers; remapped bilinearly
+    ii = ii + 1; ovars(ii)%input_var_name  = 'SSH'
+    ii = ii + 1; ovars(ii)%input_var_name  = 'SST'
+    ii = ii + 1; ovars(ii)%input_var_name  = 'SSS'
+    ii = ii + 1; ovars(ii)%input_var_name  = 'speed'
+    !ii = ii + 1; ovars(ii)%input_var_name  = 'mld'
+    ii = ii + 1; ovars(ii)%input_var_name  = 'ePBL'
+    ii = ii + 1; ovars(ii)%input_var_name  = 'MLD_003'
+    ii = ii + 1; ovars(ii)%input_var_name  = 'MLD_0125'
+
+    ! 2D fluxes with native grid location on cell centers; remapped conservatively
+    ii = ii + 1; ovars(ii)%input_var_name  = 'latent'    ; ovars(ii)%var_remapmethod  = 'conserve'
+    ii = ii + 1; ovars(ii)%input_var_name  = 'sensible'  ; ovars(ii)%var_remapmethod  = 'conserve'
+    ii = ii + 1; ovars(ii)%input_var_name  = 'SW'        ; ovars(ii)%var_remapmethod  = 'conserve'
+    ii = ii + 1; ovars(ii)%input_var_name  = 'LW'        ; ovars(ii)%var_remapmethod  = 'conserve'
+    ii = ii + 1; ovars(ii)%input_var_name  = 'evap'      ; ovars(ii)%var_remapmethod  = 'conserve'
+    ii = ii + 1; ovars(ii)%input_var_name  = 'lprec'     ; ovars(ii)%var_remapmethod  = 'conserve'
+    ii = ii + 1; ovars(ii)%input_var_name  = 'fprec'     ; ovars(ii)%var_remapmethod  = 'conserve'
+    ii = ii + 1; ovars(ii)%input_var_name  = 'LwLatSens' ; ovars(ii)%var_remapmethod  = 'conserve'
+    ii = ii + 1; ovars(ii)%input_var_name  = 'Heat_PmE'  ; ovars(ii)%var_remapmethod  = 'conserve'
+
+    ! 2D vector states on stagger locations; remapped bilinearly
+    ii = ii + 1; ovars(ii)%input_var_name  = 'SSU'
+                 ovars(ii)%var_grid = 'Cu'
+                 ovars(ii)%var_pair = 'SSV'
+                 ovars(ii)%var_pair_grid = 'Cv'
+
+    ii = ii + 1; ovars(ii)%input_var_name  = 'SSV'
+                 ovars(ii)%var_grid = 'Cv'
+                 ovars(ii)%var_pair = 'SSU'
+                 ovars(ii)%var_pair_grid = 'Cu'
+
+    ! 2D vector fluxes on stagger locations; remapped conservatively
+    ii = ii + 1; ovars(ii)%input_var_name = 'taux'
+                 ovars(ii)%var_grid = 'Cu'
+                 ovars(ii)%var_pair = 'tauy'
+                 ovars(ii)%var_pair_grid = 'Cv'
+                 ovars(ii)%var_remapmethod = 'conserve'
+
+    ii = ii + 1; ovars(ii)%input_var_name = 'tauy'
+                 ovars(ii)%var_grid = 'Cv'
+                 ovars(ii)%var_pair = 'taux'
+                 ovars(ii)%var_pair_grid = 'Cu'
+                 ovars(ii)%var_remapmethod = 'conserve'
+
+    ! 3D scalars with native grid location on cell centers; remapped bilinearly
+    ii = ii + 1; ovars(ii)%input_var_name  = 'temp' ; ovars(ii)%var_dimen = 3
+    ii = ii + 1; ovars(ii)%input_var_name  = 'so'   ; ovars(ii)%var_dimen = 3
+
+    ! 3D vectors on stagger locations; remapped bilinearly
+    ii = ii + 1; ovars(ii)%input_var_name  = 'uo'
+                 ovars(ii)%var_grid = 'Cu'
+                 ovars(ii)%var_pair = 'vo'
+                 ovars(ii)%var_pair_grid = 'Cv'
+                 ovars(ii)%var_dimen = 3
+
+    ii = ii + 1; ovars(ii)%input_var_name  = 'vo'
+                 ovars(ii)%var_grid = 'Cv'
+                 ovars(ii)%var_pair = 'uo'
+                 ovars(ii)%var_pair_grid = 'Cu'
+                 ovars(ii)%var_dimen = 3
+
+    ! set default output name
+    ovars(:)%output_var_name = ovars(:)%input_var_name
+
+  end subroutine ovars_typedefine
+end module ocnvars
